1cdfaaf2de10958738a2fe2a3b5fbf7ee7c04edd
trudy macnabb <trudy.macnabb@gmail.com>
2017-03-14 19:44:09 -0400

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,26 @@
+class PagesController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  require 'rinku'
+  def home
+  #   @requested_quote = RequestedQuote.new
+  # 	@tweets = Tweet.last(5)
+  # 	@instagrams = Gram.last(5)
+  #   @facebooks = Facebook.first(2)
+
+  # 	@socials = []
+  # 	(0..4).each do |i|
+  # 	  @socials << @tweets[i]
+  # 	  @socials << @instagrams[i]
+  # 	end
+
+  #   if params[:thing]
+  #     @first = params[:first]
+  #     @last = params[:last]
+  #     @email = params[:email]
+  #     @phone = params[:phone]
+  #     @comment = params[:comment]
+  #     RequestQuote.new_quote_request(@first, @last, @email, @phone, @comment).deliver
+  #   end  
+  end
+end
